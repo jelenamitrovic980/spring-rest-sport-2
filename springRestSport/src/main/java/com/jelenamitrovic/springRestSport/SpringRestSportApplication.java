376a668bb17092616f1a7c61388cd4762e6cf66f
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-06-30 17:31:35 +0200

    create project using springboot

---
+++ springRestSport/src/main/java/com/jelenamitrovic/springRestSport/SpringRestSportApplication.java
@@ -0,0 +1,13 @@
+package com.jelenamitrovic.springRestSport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSportApplication.class, args);
+	}
+
+}
---
+++ springRestSport/src/test/java/com/jelenamitrovic/springRestSport/SpringRestSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelenamitrovic.springRestSport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
